be460f6646836e4975017b8fc8d58219bf1dd305
Bryan Robles <bryanaka0@gmail.com>
2019-12-23 10:53:09 -0800

    improving task labeling

--- src/components/pomodoro-timer.vue
+++ src/components/pomodoro-timer.vue
@@ -3,7 +3,7 @@
         <div class="content" v-bind:class="contentClass">
             <template v-if="isRunning()">
                 <div>
-                    {{ activity }} {{ label }}
+                    {{ activity }} {{ activeLabel }}
                 </div>
                 <div>
                     <display-seconds v-bind:total-seconds="remainingSeconds"></display-seconds>
@@ -15,7 +15,12 @@
             </template>
             <template v-else-if="isStopped()">
                 <div>
-                    Just finished {{ activity }}.
+                    <template v-if="activeLabel">
+                        Just finished {{ activeLabel }}.
+                    </template>
+                    <template v-else>
+                        Just finished {{ activity }}.
+                    </template>
                 </div>
                 <div>
                     Press `Continue` to begin to {{ nextActivity }}.
@@ -56,8 +61,11 @@ enum TimerState {
 })
 export default class PomodoroTimer extends Vue {
   @Prop() private label!: string;
+  
   @Prop() private sections!: PomodoroSection[];
-
+  
+  private activeLabel!: string;
+  
   activity: string = "";
   nextActivity: string = "";
   contentClass: any = {};
@@ -77,25 +85,34 @@ export default class PomodoroTimer extends Vue {
   }
 
   start () {
-      const section = this.sections[this.methodIndex];
-      if (section) {
-        this.state = TimerState.Running;
-
-        this.activity = '';
-        if (section.timerType === TimerType.Work) {
-            this.contentClass = { working: true };
-            this.activity = 'Working on ';
-        } else if (section.timerType === TimerType.Break) {
-            this.contentClass = { onbreak: true };
-            this.activity = 'Taking a Break from ';
-        }
-
+        const section = this.sections[this.methodIndex];
+      
+        if (!section) { return; }
+        
+        this.dispatchStartSectionAction(section);
         this.remainingSeconds = section.seconds;
-        this.timerId = window.setInterval(this.tick, 1000);
+        // is bind needed?
+        this.timerId = window.setInterval(this.tick.bind(this), 1000);
+  }
+
+  finish () {
+      if(this.timerId > 0) {
+        window.clearInterval(this.timerId);
+      }
+
+      const section = this.sections[this.methodIndex];
+      if (!section) { return; }
+
+      this.dispatchStopSectionAction(section);
+
+      this.methodIndex = (this.methodIndex + 1) % this.sections.length;
+      if (this.methodIndex === 0) {
+        this.reset();
       }
   }
 
   skip () {
+      this.finish(); // why is this needed?
       this.remainingSeconds = 0;
   }
 
@@ -103,22 +120,42 @@ export default class PomodoroTimer extends Vue {
       this.remainingSeconds--;
 
       if(this.remainingSeconds <= 0) {
-          this.finishSection();
+          this.finish();
       }
   }
 
-  private finishSection () {
-      if(this.timerId > 0) {
-        window.clearInterval(this.timerId);
-      }
+  private dispatchStartSectionAction(section: PomodoroSection) {
+      switch(section.timerType) {
+        case TimerType.Work:
+            this.startWork();
+            break;
+        case TimerType.Break:
+            this.startBreak();
+            break;
+        default:
+            throw Error('something hapenned');
+    }
+  }
 
-      this.activity = '';
-      this.nextActivity = '';
+  private startWork () {
+    this.contentClass = { working: true };
+    this.activity = 'Working on ';
+    this.activeLabel = this.label;
+    // should clear label after starting task
+    this.label = '';
+    this.state = TimerState.Running;
+  }
 
-      const section = this.sections[this.methodIndex];
-      if(section) {
+  private startBreak () {
+    this.contentClass = { onbreak: true };
+    this.activity = 'Taking a Break from ';
+    this.state = TimerState.Running;
+  }
+
+  private dispatchStopSectionAction(section: PomodoroSection) {
         this.state = TimerState.Stopped;
         this.contentClass = { paused: true };
+
         if (section.timerType === TimerType.Work) {
             this.activity = 'some work';
             this.nextActivity = 'take a break';
@@ -127,13 +164,7 @@ export default class PomodoroTimer extends Vue {
             this.activity = 'taking a break';
             this.nextActivity = 'start work';
             this.currentCycle.break++;
-        }
-
-        this.methodIndex = (this.methodIndex + 1) % this.sections.length;
-        if(this.methodIndex === 0) {
-            this.reset();
-        }
-      }
+        }        
   }
 
   private reset () {
